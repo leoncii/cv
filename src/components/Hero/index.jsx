33b90e7e25da21e9d37620970204575f0bd42a10
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-09-16 21:25:13 -0400

    Hero description self-description

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,39 +1,33 @@
 import './styles.css'
 
-import { Earth } from '../svg-components/Earth'
-import { Mars } from '../svg-components/Mars'
-import { Venus } from '../svg-components/Venus'
-import { Saturn } from '../svg-components/Saturn'
-import { Neptune } from '../svg-components/Neptune'
-import { Jupiter } from '../svg-components/Jupiter'
+import Planets from '../Planets'
+import { Anchor } from '../Anchor'
 
 export function Hero () {
   return (
     <div className='hero'>
-      <div className='planets_container'>
-        <Earth />
-        <Mars />
-        <Venus />
-        <Saturn />
-        <Neptune />
-        <Jupiter />
-        <img className='cloud cloud0' src='./cloud.svg' alt='' />
-        <img className='cloud cloud1' src='./cloud.svg' alt='' />
-        <img className='cloud cloud2' src='./cloud.svg' alt='' />
-        {/* <img className='cloud cloud3' src='./cloud.svg' alt='' />
-        <img className='cloud cloud4' src='./cloud.svg' alt='' /> */}
-      </div>
-      <div className='space_container'>
-        {/* <img
-          className='floorEdit'
-          src='./floorEdit3.png'
-          alt='Esto es el fondo op ue hice para mi cv'
-        />
-        <img
-          className='spaceEdit'
-          src='./spaceEdit01.png'
-          alt=''
-        /> */}
+      <Planets />
+      <div className='hero_description'>
+        <div>
+          <h1>Leonardo Torrico</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure harum tenetur voluptas explicabo sequi accusamus in et ea porro. Doloremque cupiditate eaque reiciendis quaerat dicta. Amet nesciunt asperiores expedita omnis?</p>
+          <div>
+            <Anchor
+              href=''
+              text='Linkedin'
+            />
+            <Anchor
+              href=''
+              text='GitHub'
+            />
+          </div>
+        </div>
+        <picture>
+          <img
+            src='./googleAna.png'
+            alt=''
+          />
+        </picture>
       </div>
     </div>
   )
---
+++ src/components/Planets/index.jsx
@@ -0,0 +1,22 @@
+import { Earth } from '../svg-components/Earth'
+import { Mars } from '../svg-components/Mars'
+import { Venus } from '../svg-components/Venus'
+import { Saturn } from '../svg-components/Saturn'
+import { Neptune } from '../svg-components/Neptune'
+import { Jupiter } from '../svg-components/Jupiter'
+
+export default function Planets () {
+  return (
+    <div className='planets_container'>
+      <Earth />
+      <Mars />
+      <Venus />
+      <Saturn />
+      <Neptune />
+      <Jupiter />
+      <img className='cloud cloud0' src='./cloud.svg' alt='' />
+      <img className='cloud cloud1' src='./cloud.svg' alt='' />
+      <img className='cloud cloud2' src='./cloud.svg' alt='' />
+    </div>
+  )
+}
